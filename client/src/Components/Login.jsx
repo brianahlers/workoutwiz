ef897f62b36846aa84cf976ad630826df84b163e
Brian A <brian.ahlers@gmail.com>
2023-11-30 18:06:57 -0800

    add loginjsx page

---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform login logic here
+    console.log('Logging in...');
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
