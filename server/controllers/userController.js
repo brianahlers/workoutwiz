b60f3260378a56a996ef8889f0046dff54109485
nysat <nysa-cere3@hotmail.com>
2023-12-03 15:11:49 -0500

    changed the get all users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,16 +2,24 @@ const { User } = require('../models/User');
 
 module.exports = {
     // Get all users
-    getAllUsers(req, res) {
-        User.findAll()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(400).json(err));
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find()
+            res.json(users)
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
+    //     User.findAll()
+    //     .then((users) => res.json(users))
+    //     .catch((err) => res.status(400).json(err));
+    // },
     // Get one user
     async getUserById(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
-                .select('-__v');
+                .select('-__v')
+                .populate('Exercise'); //this is saying to populate exercise data into the user model
 
             if (!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
