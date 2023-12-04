647e40af9fea429e84c21655a59ce6e037540ff7
nysat <nysa-cere3@hotmail.com>
2023-12-04 16:42:09 -0500

    added a signup function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,4 +53,19 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    
 };
+
+//sign up user
+
+const signUp = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (err) {
+        res.status(400).json({error: error.message})
+    }
+
+
+}
