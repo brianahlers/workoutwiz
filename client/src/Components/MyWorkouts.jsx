c02e1fb864e631d257ca40b5d12efcb49939cbc4
Brian A <brian.ahlers@gmail.com>
2023-12-05 19:47:08 -0800

    add functionality to myworkouts

--- client/src/Components/MyWorkouts.jsx
+++ client/src/Components/MyWorkouts.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 const MyWorkouts = () => {
-    return (
-        <>
-            <h1>MyWorkouts</h1>
-            <h2>these are the workouts that you have tracked</h2>
+  const [workouts, setWorkouts] = useState([]);
 
-            {/* 
-            in this space we need to display the workout data from the server
-            we need it to display the workouts where we can sort by the date (newest/oldest)
-            I think we should use cards to display the workouts
-            --these things are above and beyond the requirements--
-            to be able to delete the workouts
-            to be able to edit the workouts
+  useEffect(() => {
+    // Fetch workout data from the server
+    fetch('/api/workouts')
+      .then(response => response.json())
+      .then(data => {
+        // Set the fetched workout data in state
+        setWorkouts(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-            getting the data by userid
-            */}
-            
-        </>
-    );
+  return (
+    <>
+      <h1>MyWorkouts</h1>
+      <h2>These are the workouts that you have tracked</h2>
+
+      {/* Display the workout data */}
+      {workouts.map(workout => (
+        <div key={workout.id}>
+          <h3>{workout.title}</h3>
+          <p>{workout.description}</p>
+          {/* Add additional workout details here */}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MyWorkouts;
