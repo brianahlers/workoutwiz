54ecfce690aaaabdb123fb296cf09abafa471df7
Andrea <keriazes.andrea@gmail.com>
2023-12-11 17:11:47 -0500

    edited theme page

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,5 +1,9 @@
+import * as React from 'react';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+
+
+
 
 // Create a theme instance.
 const theme = createTheme({
@@ -13,10 +17,25 @@ const theme = createTheme({
     background: {
         default: '#ECD5F1',
         },
+    typography: {
+          fontFamily: [
+            // '-apple-system',
+            // 'BlinkMacSystemFont',
+            // '"Segoe UI"',
+            // 'Roboto',
+            // '"Helvetica Neue"',
+            'Arial',
+            // 'sans-serif',
+            // '"Apple Color Emoji"',
+            // '"Segoe UI Emoji"',
+            // '"Segoe UI Symbol"',
+          ].join(','),
+        },
     error: {
       main: red.A400,
     },
   },
 });
+
 
 export default theme;
