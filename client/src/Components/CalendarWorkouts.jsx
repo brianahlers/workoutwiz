f729f824260b778a298ccf61915f1a0644e92b34
Andrea <keriazes.andrea@gmail.com>
2023-12-11 16:52:34 -0500

    added styling to my workouts and calendar workouts

--- client/src/Components/CalendarWorkouts.jsx
+++ client/src/Components/CalendarWorkouts.jsx
@@ -5,6 +5,10 @@ import { getExerciseByDate } from '../Utils/API';
 import { get } from 'mongoose';
 import { Container, Card } from 'react-bootstrap';
 import Auth from '../Utils/auth';
+import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 const CalendarWorkouts = () => {
   const [workouts, setWorkouts] = useState([]);
@@ -27,19 +31,35 @@ const CalendarWorkouts = () => {
     };
     getWorkouts();
   }, [date]);
+
   return (
+    
+<div style={{ display: 'grid', alignContent: 'center', justifyContent: 'center' }} >
     <Container className="calendarWorkouts">
-      <h1>CalendarWorkouts</h1>
-      <h2>this is where you arrive after you click on a date on the calendar</h2>
+      <Box sx={{ flexGrow: 1 }} mt={4} />
+      <h1> {date.split('00:00:00 GMT-0500 (Eastern Standard Time)')} </h1>
+      <h1>Workouts</h1>
       {workouts.map((workout) => {
         return (
-          <Container key={workout._id}>
-            <h3>{workout.title}</h3>
-            <p>{workout.sets} sets of {workout.reps} reps at {workout.weight} lbs</p>
-          </Container>
+          <Box sx={{ flexGrow: 1, mt: 2}}>
+          <Card key={workout._id} sx={{ maxWidth: 500, margin: 'auto', mt: 2, mb: 2 }} >
+            <CardContent>
+              <Typography variant='h4'>{workout.title}</Typography>
+              <Typography variant='h6'>Sets: {workout.sets} </Typography>
+              <Typography variant='h6'>Reps: {workout.reps}</Typography>
+              <Typography variant='h6'>Weight: {workout.weight} lbs</Typography>
+            </CardContent>
+          </Card>
+          </Box>
+          
+
         )
       })}
+       
     </Container>
+   
+   </div>
+
   );
 };
 
--- client/src/Components/MyWorkouts.jsx
+++ client/src/Components/MyWorkouts.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import  { CardContent, Typography,Card, Divider, } from '@mui/material';
 import { getExerciseByDate } from '../Utils/API';
 import Auth from '../Utils/auth';
+import Box from '@mui/material/Box';
+import Buttton from '@mui/material/Button';
 
 const MyWorkouts = () => {
   const [workouts, setWorkouts] = useState([]);
@@ -24,19 +26,25 @@ const MyWorkouts = () => {
 
   return (
     <>
-      <h1>MyWorkouts</h1>
-      <h2>These are the workouts that you have tracked</h2>
+    <div style={{ display: 'grid', alignContent: 'center', justifyContent: 'center' }}>
+    <Box sx={{ flexGrow: 1 }} mt={4} />
+      <h1>My Workouts</h1>
+      </div>
+      <Box sx={{ flexGrow: 1 }} mt={4} />
     {console.log(workouts)}
       {/* Display the workout data */}
       {workouts.map(workout => (
         <>
-        <Card key={workout.id}>
+        <Card key={workout.id} sx={{ maxWidth: 500, margin: 'auto', mt: 2, mb: 2}}>
+        
           <CardContent>
             <Typography variant='h4' gutterBottom>Type: {workout.title}</Typography>
             <Typography variant='h6'>Date: {workout.date}</Typography>
             <Typography  variant='h6'>Rep: {workout.reps}</Typography>
             <Typography  variant='h6'>Set: {workout.sets}</Typography>
             <Typography  variant='h6'>Weight: {workout.weight}</Typography>
+            <Buttton variant='contained' color='primary'>Edit</Buttton>
+            <Buttton variant='contained' color='primary'>Delete</Buttton>
 
 
 
@@ -44,6 +52,7 @@ const MyWorkouts = () => {
   
           </CardContent>
         </Card>
+        <Box sx={{ flexGrow: 1 }} mt={2} />
                 <Divider variant='middle'/>
         </>
       ))}
