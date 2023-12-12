892fde5f6f791ba2be68c662dc33135a3f9d9508
Andrea <keriazes.andrea@gmail.com>
2023-12-12 17:53:13 -0500

    edited thank you page

--- client/src/Components/ThankYou.jsx
+++ client/src/Components/ThankYou.jsx
@@ -2,7 +2,9 @@
 export default function ThankYou(){
     return(
         <div className="ThankYou">
+            <br />
             <h1>Thank you for your submission! </h1>
+            <br />
             <h2>A certified person trainer is reviewing your question and will be in contact with you soon!</h2>
         </div>
     )
