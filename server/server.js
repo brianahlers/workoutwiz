5084bdfb54aa9e7e0ec958770ea7e895758e0d13
Brian A <brian.ahlers@gmail.com>
2023-12-12 18:53:02 -0800

    troubleshoot server js line 17 18

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+//   app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// });
 }
 
 
