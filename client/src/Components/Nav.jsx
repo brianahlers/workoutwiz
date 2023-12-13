64fa657ce04370a7ae86bce39ff52bef28795d7b
Brian A <brian.ahlers@gmail.com>
2023-12-12 20:36:33 -0800

    troubleshooting

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -26,13 +26,13 @@ const Nav = () => {
     <AppBar position="static">
 <Typography variant="h6" component="div" sx={{margin: 'auto', fontSize: '1rem'}}> <img src={logo} alt="Workout Wiz Logo" className="logo" width="350" height="250" /> </Typography>
     <Toolbar>
-      {!Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/">Login</Button>}
+      {/* {!Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/">Login</Button>}
       {Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/home" sx={{ mr: 3 }}>Home</Button> }
       {Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/myworkouts" sx={{ mr: 3 }}>My Workouts</Button> }
       {Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/newworkout" sx={{ mr: 3 }}>Add Workout</Button> }
       {Auth.loggedIn() && <Button color="inherit" component={RouterLink} to="/ask" sx={{ mr: 3 }}>Ask A Certified Personal Trainer</Button> }
       {Auth.loggedIn() && <Box sx={{ flexGrow: 1 }} />}
-      {Auth.loggedIn() && <Button color="inherit" onClick={handleLogout}>Logout</Button> }
+      {Auth.loggedIn() && <Button color="inherit" onClick={handleLogout}>Logout</Button> } */}
     </Toolbar>
     
   </AppBar>
