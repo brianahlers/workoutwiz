2ce4798fd1804838fed55246dfb655d65fe7e351
Brian A <brian.ahlers@gmail.com>
2023-12-12 18:46:13 -0800

    troubleshoot path serverjs line 16 and 18 should be dist

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 }
 
