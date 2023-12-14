aa5e2f79d2e99120d8b26a4b40c9c64b9f1c7d95
Brian A <brian.ahlers@gmail.com>
2023-12-13 18:11:46 -0800

    testing index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+
 
 router.use('/api', apiRoutes);
 // router.use('/', homeRoutes);
